b9b9458cdd6df825ecdf6c3f67a443e5eb58a190
Rahul <84670032+rahulb813@users.noreply.github.com>
2021-10-31 16:32:11 +0530

    Add files via upload

---
+++ week_6-q2.js
@@ -0,0 +1,9 @@
+var res = 813;
+document.write("Here a variable 'res' holds a numeric value and it is: " + res +  "<br/>");
+res = "A text string";
+document.write("Now the same variable holds a string value and it is: " + res + "<br/>");
+res = '&';
+document.write("Now the same variable holds a character value and it is: " + res + "<br/>");
+res = true;
+document.write("Now the same variable holds a boolean value and it is: " + res + "<br/>");
+document.write("General mathematical applications and string operations can be done in the usual ways...");
