eef93e2b9766517b68f61931f4a1fe036755d84c
Rahul <84670032+rahulb813@users.noreply.github.com>
2021-10-31 17:19:00 +0530

    Add files via upload

---
+++ week_6-q3.js
@@ -0,0 +1,10 @@
+var a = 7;
+document.write("Here a varible 'a' is created and it holds a value " + a + "<br/>");
+var b;
+document.write("Here another variable 'b' is created and no value is given...<br/>");
+document.write("Hence it gives out value as " + b + "<br/>");
+document.write("We saw the keyword 'undefined' when we tried to print out the variable, hence it is an undefined variable.<br/>");
+document.write("Now we did not created any variable and we try to print its value now in the next line...<br/>");
+document.write(x + "<br/>");
+document.write("We see we didn't got anything because that variable did not exist and hence there created an error.")
+document.write("Due to that error, the execution of remaining code has been stopped.<br/>");
